42af74280d5688fcfa8bce46f721ba55e3f4f7e5
Max van der Stam <max@hellodialog.com>
2017-03-27 14:45:10 +0200

    Update Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Mvdstam\Oauth2ServerLaravel\Http\Middleware\OAuth2Middleware;
 
 class Kernel extends HttpKernel
 {
@@ -11,14 +12,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
-    ];
+    protected $middleware = [];
 
     /**
      * The application's route middleware.
@@ -26,8 +20,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'oauth2' => OAuth2Middleware::class
     ];
 }
