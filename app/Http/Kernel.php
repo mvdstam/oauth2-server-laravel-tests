782502c299d81e368c5ce2be30e26a0c3e2a96b7
Max van der Stam <max@hellodialog.com>
2017-03-28 16:42:26 +0200

    Add StartSession middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Session\Middleware\StartSession;
 use Mvdstam\Oauth2ServerLaravel\Http\Middleware\OAuth2Middleware;
 
 class Kernel extends HttpKernel
@@ -12,7 +13,9 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middleware = [];
+    protected $middleware = [
+        StartSession::class
+    ];
 
     /**
      * The application's route middleware.
